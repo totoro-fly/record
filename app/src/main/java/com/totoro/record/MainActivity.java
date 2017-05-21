31e5f3456ceb82eb8b4c6a26993563636f59bb78
totoro-fly <liujian180389@gmail.com>
2017-05-21 14:14:19 +0800

    不想改

--- app/src/main/java/com/totoro/record/MainActivity.java
+++ app/src/main/java/com/totoro/record/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayDatabaseInfo() {
+        
         SQLiteDatabase db = recordDbHelper.getReadableDatabase();
         Cursor cursor = recordDbHelper.readAllData(db);
         try {
